77d5486315a5729afc0c5df074515c48e4d23cac
05284 <oscarbrighton@zetech.ac.ke>
2023-09-13 14:20:28 -0700

    Updated

---
+++ 100-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * This program calls the print_times_table function with different arguments.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);
+    return (0);
+}
